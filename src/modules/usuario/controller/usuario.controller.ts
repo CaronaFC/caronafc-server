a588e3b644823c23410126a39d2fae60bc853cfc
Renan Freitas <renan.freitas.anjos@gmail.com>
2025-06-15 19:49:57 -0300

    config: swagger router

--- src/modules/usuario/controller/usuario.controller.ts
+++ src/modules/usuario/controller/usuario.controller.ts
@@ -1,8 +1,10 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CreateUsuarioDto } from '../dto/create-usuario.dto';
 import { UpdateUsuarioDto } from '../dto/update-usuario.dto';
 import { UsuarioService } from '../services/usuario.service';
 
+@ApiTags('usuario')
 @Controller('usuario')
 export class UsuarioController {
   constructor(private readonly usuarioService: UsuarioService) {}
@@ -13,6 +15,8 @@ export class UsuarioController {
   }
 
   @Get()
+  @ApiOperation({ summary: 'Todos os usuários' })
+  @ApiResponse({ status: 200, description: 'Lista de todos os usuários'})
   findAll() {
     return this.usuarioService.findAll();
   }
