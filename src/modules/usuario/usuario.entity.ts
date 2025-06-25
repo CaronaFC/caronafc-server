227645f879a50ee6cddfd375a751e318a47e9b4f
MatheusVinicius77 <matheusviniciusbus7@hotmail.com>
2025-06-25 03:03:02 -0300

    fix: add right dto names

--- src/modules/usuario/usuario.entity.ts
+++ src/modules/usuario/usuario.entity.ts
@@ -1,9 +1,11 @@
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { BeforeInsert, Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { Avaliacao } from '../avaliacao/avaliacao.entity';
 import { Veiculo } from '../veiculo/veiculo.entity';
 
+import * as bycrypt from "bcrypt"
+
 @Entity('usuario')
-export class Usuario{
+export class Usuario {
 
     @PrimaryGeneratedColumn()
     id: number;
@@ -14,27 +16,32 @@ export class Usuario{
     @Column({ nullable: false })
     email: string;
 
-    @Column({ nullable: false})
+    @Column({ nullable: false })
     numero: string;
 
     @Column({ nullable: false })
     cpf: string;
-    
+
     @Column({ nullable: false })
     senha: string;
-    
+
     @Column({ nullable: true, type: 'varchar', length: 255 })
     imagem: string;
 
-    @Column({ type: 'date', nullable: false})
+    @Column({ type: 'date', nullable: true })
     data_nascimento: Date;
 
     @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
     data_criacao: Date;
 
-    @OneToMany(()=> Avaliacao, avaliacao => avaliacao.usuario_reportado, {nullable: true})
+    @OneToMany(() => Avaliacao, avaliacao => avaliacao.usuario_reportado, { nullable: true })
     avaliacoes: Avaliacao[];
 
-    @OneToMany(()=> Veiculo, veiculo=> veiculo.usuario, {nullable: true} )
+    @OneToMany(() => Veiculo, veiculo => veiculo.usuario, { nullable: true })
     veiculos: Veiculo[];
+
+    @BeforeInsert()
+    async hashPassword() {
+        this.senha = await bycrypt.hash(this.senha, 10);
+    }
 }
