df6db7c1a010b028806fd3731b76fb61f856f83f
MatheusVinicius77 <matheusviniciusbus7@hotmail.com>
2025-07-08 07:10:49 -0300

    feat: add viagem principal methods

---
+++ src/modules/viagem/dto/create-viagem.dto.ts
@@ -0,0 +1,29 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { CreateJogoDto } from '../../jogo/dto/create-jogo.dto';
+
+export class CreateViagemDto {
+    @ApiProperty()
+    motoristaId: number;
+
+    @ApiProperty({ type: CreateJogoDto })
+    jogo: CreateJogoDto;
+
+    @ApiProperty()
+    origem_lat: number;
+
+    @ApiProperty()
+    origem_long: number;
+
+    @ApiProperty({ type: String, example: '2025-07-12T22:00:00Z' })
+    horario: Date;
+
+    @ApiProperty()
+    qtdVagas: number;
+
+
+    @ApiProperty({ example: true })
+    temRetorno: boolean;
+
+    @ApiProperty({ example: 15.00 })
+    valorPorPessoa: number;
+}
