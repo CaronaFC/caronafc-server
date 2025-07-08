c77be6f4f29f6dc6271b15b8c75db73f170dadaf
MatheusVinicius77 <matheusviniciusbus7@hotmail.com>
2025-07-08 07:09:45 -0300

    chore: add swagger examples schema to usuario

--- src/modules/usuario/usuario.entity.ts
+++ src/modules/usuario/usuario.entity.ts
@@ -2,8 +2,7 @@ import { BeforeInsert, Column, Entity, OneToMany, PrimaryGeneratedColumn } from 
 import { Avaliacao } from '../avaliacao/avaliacao.entity';
 import { Veiculo } from '../veiculo/veiculo.entity';
 
-import * as bycrypt from "bcrypt"
-
+import * as bcrypt from "bcrypt";
 @Entity('usuario')
 export class Usuario {
 
@@ -45,6 +44,6 @@ export class Usuario {
 
     @BeforeInsert()
     async hashPassword() {
-        this.senha = await bycrypt.hash(this.senha, 10);
+        this.senha = await bcrypt.hash(this.senha, 10);
     }
 }
