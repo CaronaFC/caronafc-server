bdd66684ab203255b9549bfbff9b706db9a26c57
MatheusVinicius77 <matheusviniciusbus7@hotmail.com>
2025-07-08 07:10:33 -0300

    feat: add jogo principal services

--- src/modules/jogo/services/jogo.service.ts
+++ src/modules/jogo/services/jogo.service.ts
@@ -1,30 +1,18 @@
 import { Injectable } from '@nestjs/common';
-
+import * as fs from 'fs';
+import * as path from 'path';
 
 @Injectable()
 export class JogoService {
     // Implementação dos métodos do serviço de Jogo
     // Exemplo: listarJogos
     // Esses métodos irão interagir com uma api que irá buscar os dados de jogos.
-    
-    async listarJogos(): Promise<any[]> {
-        // Aqui você implementaria a lógica para buscar os jogos
-        //  chamda de API localhost:3000/matches
-        try {
-            const response = await fetch('https://api.soccerdataapi.com/matches/?league_id=215&auth_token=bcaa4c172d7604813d797d51aad6decc7d623cfd');
-            if (!response.ok) {
-                throw new Error('Erro ao buscar jogos');
-            }
-            // verificar se a data do jogo é menor que a data atual
-            const jogos = await response.json();
-            const dataAtual = new Date();
-            const jogosFiltrados = jogos.filter(jogo => new Date(jogo.horario) > dataAtual);
-            // ordenar os jogos pela data do jogo
-            return jogosFiltrados;
-        } catch (error) {
-            console.error('Erro ao listar jogos:', error);
-            return [];
-        }
+
+    async listarJogos() {
+        const filePath = path.resolve(process.cwd(), 'filtered_matches.json');
+        const rawData = fs.readFileSync(filePath, 'utf-8');
+        const jogos = JSON.parse(rawData);
+        return jogos;
     }
 
     async buscarTimePorId(id: number): Promise<any> {
